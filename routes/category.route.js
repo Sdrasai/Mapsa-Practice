55d0a621a26b87674e31e71e551c11a49450ea4d
Sadra Sadeghi <118170146+Sdrasai@users.noreply.github.com>
2023-12-24 14:28:51 +0330

    Delete routes directory

--- routes/category.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const { categoryController } = require("../controllers")
-const router = express.Router()
-
-router.post("/createCategory", categoryController.createCategory)
-router.put("/updateCategory/:categoryId", categoryController.updateCategory)
-
-module.exports = router
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const productRoute = require("./product.route")
-const categoryRoute = require("./category.route")
-
-const router = express.Router()
-
-router.use("/product", productRoute, categoryRoute)
-
-module.exports = router
